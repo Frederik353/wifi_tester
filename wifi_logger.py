4e3a781b7686354a4f3cb74caf6dc2cfb3cb7bd1
Frederik353 <frederik.negaard@gmail.com>
2021-03-22 18:24:51 +0100

    commenting

--- wifi_logger.py
+++ wifi_logger.py
@@ -1,18 +1,20 @@
+# importerer nødvnedige library'er
+
 import sys
 import os
 import platform
 import ipaddress
 import asyncio
-from datetime import datetime as dt 
+from datetime import datetime as dt
 import speedtest
 
-
+# erklerer globale variabler
 my_list_of_tasks = []
 my_tasks = []
 nbr_host_found = 0
 list_of_hosts_found = []
 
-
+# asynkron funksjon for å send ping til spesifiser ip på lokalt nettverk
 async def ping_coroutine(cmd, ip):
 
    global nbr_host_found, list_of_hosts_found
@@ -22,8 +24,11 @@ async def ping_coroutine(cmd, ip):
       stdout=asyncio.subprocess.PIPE,
       stderr=asyncio.subprocess.PIPE)
 
+   # vent for svar på ping 
    stdout = await running_coroutine.communicate()
 
+   # hvis svar inneholder time to live inkluder ip i liste 
+   # og inkrementer antall enheter funnet
    if "ttl=" in str(stdout).lower():
 
       nbr_host_found += 1
@@ -41,12 +46,14 @@ async def ping_loop():
 class Networkscan:
 
    def __init__(self, ip_and_prefix):
-
+      # deklarere verdier brukt i objekt
       self.nbr_host_found = 0
       self.list_of_hosts_found = []
 
+      # fil hvor lokale ip adresser funnet blir lagret
       self.filename = "hosts.txt"
 
+      # tester om ip adresse er riktig
       try:
             self.network = ipaddress.ip_network(ip_and_prefix)
       except:
@@ -57,24 +64,19 @@ class Networkscan:
       if self.network.num_addresses > 2:
             self.nbr_host -= 2
 
+      # kommando som skrives inn i terminal
       self.one_ping_param = "ping -n 1 -l 1 -w 1000 " if platform.system().lower() == "windows" else "ping -c 1 -s 1 -w 1 "
 
+   # funksjon for å skrive inn data i fil
    def write_file(self, filename="speedtest.csv"):
       ret = 0
-
-      # data = ""
-      # for i in self.list_of_hosts_found:
-      #    data += i + "\n"
-         # with open(filename, "w") as f:
-         #    f.write(data)
-
       now = dt.now()
       time = now.strftime("%d/%m/%Y,%T")
       try:
          with open(filename, "a") as f:
                if os.stat(filename).st_size == 0:
-                  f.write("Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s),number of hosts online\n")
-               f.write(f"{time},{self.download},{self.upload},{self.ping},{self.nbr_host_found}\n")
+                  f.write("Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s),number of hosts online, url, latitude, longitude, location, country, countrycode, ISP, id, host, d, latency \n")
+               f.write(f"{time},{self.download},{self.upload},{self.ping},{self.nbr_host_found},{self.server}\n")
 
       except Exception:
          ret = 1
@@ -84,12 +86,17 @@ class Networkscan:
    def run(self):
       global my_tasks, nbr_host_found, list_of_hosts_found, my_list_of_tasks
 
-      # speedtest
+      # kjører speedtest
       st = speedtest.Speedtest()
-
+      # lagrer resultater
+      # merk download og upload i multithread mode
       self.download = st.download()
       self.upload = st.upload()
       self.ping = st.results.ping
+      # self.server = str(st.get_best_server()).replace(",", "     ")
+      self.server = list(st.get_best_server().values())
+
+
 
       # check for hosts
       self.nbr_host_found = 0
@@ -119,8 +126,7 @@ class Networkscan:
                cmd = self.one_ping_param + host
                my_tasks.append(ping_coroutine(cmd, host))
 
-         # print(str(len(my_list_of_tasks)))
-
+         # hvis kjøres på vindows kjør asyncio loop'er med IOCP
          if platform.system().lower() == "windows":
             asyncio.set_event_loop_policy(
             asyncio.WindowsProactorEventLoopPolicy())
@@ -132,22 +138,22 @@ class Networkscan:
 
 
 
-
-
-
 if __name__ == '__main__':
 
+   # lokal ip adresse og cidr notasjon for subnet maske
    my_network = "192.168.10.0/24"
+
 
    my_scan = Networkscan(my_network)
 
+
+   # skriv inn data i csv fil og  print ut masse data for debugging
    print("Network to scan: " + str(my_scan.network))
    print("Prefix to scan: " + str(my_scan.network.prefixlen))
    print("Number of hosts to scan: " + str(my_scan.nbr_host))
    print("Scanning hosts...")
 
    my_scan.run()
-
 
    print("List of hosts found:")
 
